8865f6a9aa56a26bf0096aa5045326ec69d443dc
Люба <gareevaliubov@gmail.com>
2024-09-27 12:44:03 +0300

    поправила разметку

--- src/components/SearchAlgorithm.vue
+++ src/components/SearchAlgorithm.vue
@@ -17,55 +17,57 @@ const options = ref([
     { text: 'Выберите один из вариантов', value: 'start' },
     { text: 'Линейный поиск', value: 'linearSearch' },
     { text: 'Бинарный поиск', value: 'binarySearch' }
-    
+
 ])
 
 function searchStart() {
 
-arraySearchs = inputString.value.split(' ').map(str => parseFloat(str));
-const searchAlgorithm = SearchAlgorithmFactory.getAlgorithm(selected.value);
+    toggle.value = true;
 
-if(searchNumber.value == null) {
-    throw Error("Число поиска не заполнено");
-}
+    arraySearchs = inputString.value.split(' ').map(str => parseFloat(str));
+    const searchAlgorithm = SearchAlgorithmFactory.getAlgorithm(selected.value);
 
-let answer = searchAlgorithm.search(arraySearchs, searchNumber.value) as number;
+    if (searchNumber.value == null) {
+        throw Error("Число поиска не заполнено");
+    }
+
+    let answer = searchAlgorithm.search(arraySearchs, searchNumber.value) as number;
 
 
 
-if (selected.value == 'binarySearch') {
-    
-    let arr = inputString.value.split(' ').map(str => parseFloat(str));
-    
-    sort(arr);
+    if (selected.value == 'binarySearch') {
 
-    if (arr != arraySearchs) {
-        inputString.value = arr.join( );
-        toggle.value = true;
+        let arr = inputString.value.split(' ').map(str => parseFloat(str));
+
+        sort(arr);
+
+        if (arr != arraySearchs) {
+            inputString.value = arr.join();
+            toggle.value = true;
+        }
+        else {
+            toggle.value = false;
+        }
+    }
+
+    if (answer != -1) {
+        result.value = `Индекс искомого значения равен: ${answer}`;
     }
     else {
-        toggle.value = false;
+        result.value = 'Индекс не найден';
     }
-}
-
-if (answer != -1) {
-    result.value = `Индекс искомого значения равен: ${answer}`;
-}
-else {
-    result.value = 'Индекс не найден';
-}
 }
 
 
 function sort(array: number[]): void {
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 0; j < array.length - i; j++) {
-              if (array[j] > array[j + 1]) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i; j++) {
+            if (array[j] > array[j + 1]) {
                 [array[j], array[j + 1]] = [array[j + 1], array[j]];
-              }
             }
-          }
+        }
     }
+}
 </script>
 
 <template>
@@ -79,48 +81,49 @@ function sort(array: number[]): void {
                         Алгоритмы поиска
                     </div>
                     <form class="card-body" novalidate>
-                        <div class="row row__padding">
-                            <div class="col-auto col__sizing">
-                                <label for="inputControl01" class="card-text w-70 card-text__margin">Введите данные
-                                через пробел:</label>
-                            <input id="inputControl01" class="form-control" v-model="inputString"
-                            :class=" [ {'is-invalid': toggle == true && selected == 'binarySearch' } ]"
-                            >
-                            <div id="validationServerUsernameFeedback" class="invalid-feedback invalid__size">
-                                <div class="invalid__width">
-        Пожалуйста, введите отсортированный массив для применения бинарного поиска.
-      </div>
-
-      <div v-if="toggle" class="row row__position-w">
-        <button type="button" class="btn btn-info button__width">Отсортировать?</button>
-      </div>
+                        <div>
+                            <div class="row row__padding">
+                                <div class="col-8 col__sizing">
+                                    <label for="inputControl01" class="card-text w-70 card-text__margin">Введите данные
+                                        через пробел:</label>
+                                    <input id="inputControl01" class="form-control" v-model="inputString"
+                                        :class="[{ 'is-invalid': toggle == true && selected == 'binarySearch' }]">
+                                    <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                                        <div>Бинарный поиск работает только с отсортированным массивом.</div>
+                                    </div>
+                                </div>
+                                <div class="col-4 padding">
+                                    <label for="inputControl02"
+                                        class="card-text w-55 card-text__margin">Элемент:</label>
+                                    <input id="inputControl02" type="number" class="form-control"
+                                        v-model="searchNumber">
+                                </div>
                             </div>
-      
-      
-                        </div>
-                            <div class="col padding">
-                                <label for="inputControl02" class="card-text w-30 card-text__margin">Ищем элемент:</label>
-                                <input id="inputControl02" type="number" class="form-control" v-model="searchNumber">
-                            </div>
-                        </div>
 
-                        <div class="row row__position">
-                            <select v-model="selected" class="form-select input__width"
-                                aria-label="Выбор алгоритма поиска">
-                                <option v-for="option in options" :value="option.value">
-                                    {{ option.text }}
-                                </option>
-                            </select>
-                        </div>
-                        <div class="row row__position">
-                            <button type="button" class="btn btn-primary button__width"
-                                :disabled="(selected == 'start' || searchNumber == null) ? true : false" @click="searchStart">
-                                Найти
-                            </button>
-                        </div>
-                        <div class="row row__position row__bottom">
-                            <label for="inputControl03" class="card-text card-text__margin">Результат:</label>
-                            <input id="inputControl03" class="form-control input__width" :value="result" readonly>
+                            <div v-if="toggle" class="row row__position">
+                                <div class="col-12">
+                                    <button type="button" class="btn btn-info" style="width: 366px;">Отсортировать?</button>
+                                </div>
+                            </div>
+                            <div class="row row__position">
+                                <select v-model="selected" class="form-select input__width"
+                                    aria-label="Выбор алгоритма поиска">
+                                    <option v-for="option in options" :value="option.value">
+                                        {{ option.text }}
+                                    </option>
+                                </select>
+                            </div>
+                            <div class="row row__position">
+                                <button type="button" class="btn btn-primary button__width"
+                                    :disabled="(selected == 'start' || searchNumber == null) ? true : false"
+                                    @click="searchStart">
+                                    Найти
+                                </button>
+                            </div>
+                            <div class="row row__position row__bottom">
+                                <label for="inputControl03" class="card-text card-text__margin">Результат:</label>
+                                <input id="inputControl03" class="form-control input__width" :value="result" readonly>
+                            </div>
                         </div>
                     </form>
                 </div>
@@ -130,7 +133,6 @@ function sort(array: number[]): void {
 </template>
 
 <style scoped>
-
 .container__margin {
     margin-top: 100px;
 }
@@ -147,13 +149,13 @@ function sort(array: number[]): void {
 
 .input__width,
 .button__width {
-   margin: auto 10px;
-   width: 100%;
-   margin-left: 13px;
+    margin: auto 10px;
+    width: 100%;
+    margin-left: 13px;
 }
 
 .row__bottom {
-   margin-bottom: 10px; 
+    margin-bottom: 10px;
 }
 
 .col__sizing {
